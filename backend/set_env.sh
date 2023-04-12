6057df0e7aaa1e7034e020e637f4d731286c8aa9
a.mahdi <a.elmahdy.mail@gmail.com>
2023-04-12 23:36:15 +0200

    Test

---
+++ backend/set_env.sh
@@ -0,0 +1,11 @@
+export NODE_ENV=production
+export VERSION=1
+export TYPEORM_CONNECTION=postgres
+export TYPEORM_MIGRATIONS_DIR=./migrations
+export TYPEORM_ENTITIES=./modules/domain/**/*.entity.js
+export TYPEORM_MIGRATIONS=./src/migrations/*.js
+export TYPEORM_HOST=localhost
+export TYPEORM_PORT=5432
+export TYPEORM_USERNAME=postgres
+export TYPEORM_PASSWORD=Password2
+export TYPEORM_DATABASE=glee
